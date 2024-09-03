12729a7342dab797238d20671fa14ae810db055d
najulas <133673937+najulas@users.noreply.github.com>
2024-09-03 23:06:39 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const primeiroNome = "Naju";
+const sobrenome = "Meira";
+const idade = 16;
+const cidadeNatal = "Campo Mourão";
+const altura = 1.67;
+
+console.log("O meu primeiro nome é")
+console.log(primeiroNome);
+console.log("O meu sobrenome é")
+console.log(sobrenome);
+console.log("A minha idade é")
+console.log(idade);
+console.log("A minha cidade natal é")
+console.log(cidadeNatal);
+console.log("A minha altura é")
+console.log(altura);
